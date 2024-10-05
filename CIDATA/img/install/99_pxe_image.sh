1c2af429b23852dbf0bcfc7830ddf5e31e43e85e
cloud-init-metal-deploy <cloud-init-metal-deploy>
2024-10-05 05:15:11 +0200

    sync one last time before mksquashfs execution

--- CIDATA/img/install/99_pxe_image.sh
+++ CIDATA/img/install/99_pxe_image.sh
@@ -11,6 +11,7 @@ exec &> >(while IFS=$'\r' read -ra line; do [ -z "${line[@]}" ] && line=( '' ); 
 # create a squashfs snapshot based on rootfs
 LC_ALL=C yes | LC_ALL=C pacman -S --noconfirm --needed squashfs-tools
 mkdir -p /share/pxe/arch/x86_64
+sync
 mksquashfs / /share/pxe/arch/x86_64/pxeboot.img -comp zstd -Xcompression-level 4 -b 1M -progress -wildcards \
   -e "boot/*" "cidata*" "dev/*" "etc/fstab" "etc/crypttab" "etc/crypttab.initramfs" "proc/*" "sys/*" "run/*" "mnt/*" "share/*" "media/*" "tmp/*" "var/tmp/*" "var/log/*" "var/cache/pacman/pkg/*"
 
