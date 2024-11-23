346825dadbe9d481cccccbc0c3568edcbdfc2265
cloud-init-metal-deploy <cloud-init-metal-deploy>
2024-11-23 03:54:48 +0100

    fixing iscsi pxe boot

--- CIDATA/img/install/99_pxe_image.sh
+++ CIDATA/img/install/99_pxe_image.sh
@@ -5,15 +5,23 @@ systemctl mask systemd-network-generator
 
 # create a squashfs snapshot based on rootfs
 LC_ALL=C yes | LC_ALL=C pacman -S --noconfirm --needed squashfs-tools
-mkdir -p /share/pxe/arch/x86_64
+mkdir -p /srv/pxe/arch/x86_64
 sync
-mksquashfs / /share/pxe/arch/x86_64/pxeboot.img -comp zstd -Xcompression-level 4 -b 1M -progress -wildcards \
-  -e "boot/*" "cidata*" "dev/*" "etc/fstab" "etc/crypttab" "etc/crypttab.initramfs" "proc/*" "sys/*" "run/*" "mnt/*" "share/*" "media/*" "tmp/*" "var/tmp/*" "var/log/*" "var/cache/pacman/pkg/*"
+mksquashfs / /srv/pxe/arch/x86_64/pxeboot.img -comp zstd -Xcompression-level 4 -b 1M -progress -wildcards \
+  -e "boot/*" "cidata*" "dev/*" "etc/fstab" "etc/crypttab" "etc/crypttab.initramfs" "proc/*" "sys/*" "run/*" "mnt/*" "share/*" "srv/pxe/*" "media/*" "tmp/*" "var/tmp/*" "var/log/*" "var/cache/pacman/pkg/*"
 
 # reenable systemd-network-generator
 systemctl unmask systemd-network-generator
 
 LC_ALL=C yes | LC_ALL=C pacman -S --noconfirm --needed mkinitcpio-nfs-utils curl ca-certificates-utils cifs-utils nfs-utils nbd open-iscsi nvme-cli
+
+# configuring iscsi
+sed -e 's/^node.conn[0].timeo.noop_out_interval.*/node.conn[0].timeo.noop_out_interval = 0/' \
+    -e 's/^node.conn[0].timeo.noop_out_timeout.*/node.conn[0].timeo.noop_out_timeout = 0/' \
+    -e 's/^node.session.timeo.replacement_timeout.*/node.session.timeo.replacement_timeout = 86400/' -i /etc/iscsi/iscsid.conf
+tee /etc/udev/rules.d/50-iscsi.rules <<'EOF'
+ACTION=="add", SUBSYSTEM=="scsi" , ATTR{type}=="0|7|14", RUN+="/bin/sh -c 'echo Y > /sys$$DEVPATH/timeout'"
+EOF
 
 echo ":: create skeleton for pxe boot mkinitcpio"
 mkdir -p /etc/initcpio/{install,hooks}
@@ -28,5 +36,6 @@ cp /cidata/install/pxe/pxe.preset /etc/mkinitcpio.d/
 echo ":: create pxe boot initcpio"
 mkdir -p /var/tmp/mkinitcpio
 mkinitcpio -p pxe -t /var/tmp/mkinitcpio
-cp /boot/vmlinuz-linux /boot/initramfs-linux-pxe.img /share/pxe/arch/x86_64/
-chmod 644 /share/pxe/arch/x86_64/*
+cp /boot/vmlinuz-linux /boot/initramfs-linux-pxe.img /srv/pxe/arch/x86_64/
+chmod 644 /srv/pxe/arch/x86_64/*
+chown root:root /srv/pxe/arch/x86_64/*
